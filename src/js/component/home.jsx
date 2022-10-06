930a2842a4c4776d0215ca61e03a0d8e702d68f3
Sami <sandramilenamartinezarrieta@hotmail.com>
2022-10-06 15:59:47 +0000

    toDoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [tarealistita, setTarea] = useState("");
+  const [listaDeTareas, setListadeTareas] = useState([]);
+  let eliminar = []
+
+  function subirlista(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      setListadeTareas([...listaDeTareas, tarealistita]);
+      setTarea("");
+      console.log(tarealistita);
+    }
+  }
+    function deleteItem(index) {
+    // crear la variable que recibe la lista
+      
+      eliminar= listaDeTareas.filter((item, i) =>{
+        if(index !==i){
+          return item
+        }
+// forma comprimida: eliminar = lisTareas,filter((item,index)=>index !==id))
+// forma comprimida con set : setListaTareas(lisTareas,filter((item,index)=>index !==id))
+
+              })
+              setListadeTareas(eliminar)
+    }
+  
+  
+
+  return (
+    <div className="text-center">
+      <div className="d-flex justify-content-start p-3 m-3 col-12">
+        <div className="m-3 col-2">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            TO DO LIST
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="List"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarealistita}
+            onKeyPress={subirlista}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder= {"te faltan "+[listaDeTareas.length]+" items"}
+            disabled
+          />
+        </div>
+        <br />
+        <div className="d-flex flex-column-start col-9">
+          <div className="col-7" style={{   backgroundImage: `url("https://st.depositphotos.com/1427101/4993/v/600/depositphotos_49934977-stock-illustration-notebook-a4-size.jpg")`,height: "500px",width: "1000px", backgroundRepeat: "no-repeat"}}>
+            <ul className="list-group text-start"style={{marginLeft: "5em", marginTop: "5em"}}>
+            <div className="col-6 align-middle">
+              {listaDeTareas.map((item, i) => (
+                <li className="list-group-item d-flex justify-content-between align-items-center" key={i}>
+                  {item} &nbsp;&nbsp;&nbsp;
+                  <button className=" btn btn-primary rounded-pill float-end" onClick={() => deleteItem(i)}>
+                    X
+                  </button>
+                </li>
+              ))}
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
